a9c1aca9d819d5bc98e3d4a35b6aa809dd3a3e2c
Grzegorz <slowiaczek.grzegorz@gmail.com>
2025-07-31 11:27:36 +0200

    Dodanie projektu do portfolio

--- src/app/projekty/page.tsx
+++ src/app/projekty/page.tsx
@@ -51,20 +51,6 @@ export default function Projekty() {
         </section>
 
         <section className={styles.gallery} id="firstProject">
-          <div className={styles.galleryGrid}>
-            <Image
-              src="/images/portfolio/projekt-strony-gabinet-logopedyczny-responsive.webp"
-              alt="Responsywny projekt strony internetowej gabinetu logopedycznego dla dzieci i młodzieży, wyświetlany na różnych urządzeniach: komputerze, laptopie, tablecie i smartfonie"
-              width="2085"
-              height="1527"
-              loading="lazy"
-            />
-            <div className={styles.gallerytext}>
-              <a href="https://logopedycznagloska.pl/" target="_blank" rel="noopener noreferrer">
-                <FaArrowRight className={styles.strzalka} /> Zobacz pełną wersję strony
-              </a>
-            </div>
-          </div>
 
           <div className={styles.galleryGrid}>
             <Image
@@ -196,6 +182,21 @@ export default function Projekty() {
             />
             <div className={styles.gallerytext}>
               <a href="https://exdach.com/" target="_blank" rel="noopener noreferrer">
+                <FaArrowRight className={styles.strzalka} /> Zobacz pełną wersję strony
+              </a>
+            </div>
+          </div>
+
+          <div className={styles.galleryGrid}>
+            <Image
+              src="/images/portfolio/projekt-strony-gabinet-logopedyczny-responsive.webp"
+              alt="Responsywny projekt strony internetowej gabinetu logopedycznego dla dzieci i młodzieży, wyświetlany na różnych urządzeniach: komputerze, laptopie, tablecie i smartfonie"
+              width="2085"
+              height="1527"
+              loading="lazy"
+            />
+            <div className={styles.gallerytext}>
+              <a href="https://logopedycznagloska.pl/" target="_blank" rel="noopener noreferrer">
                 <FaArrowRight className={styles.strzalka} /> Zobacz pełną wersję strony
               </a>
             </div>
